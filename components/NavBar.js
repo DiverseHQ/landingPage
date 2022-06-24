0fe41b8b525a98a1252fdbf8354f09588c85a316
RathodDeven <devenrathodrd@gmail.com>
2022-06-24 18:51:57 +0530

    discord and twitter link

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
 const NavBar = () => {
+  const openInNewTab = (url) => {
+    const win = window.open(url, '_blank')
+    win.focus()
+  }
   return (
-    <div className='flex flex-row justify-end items-center p-2 h-[60px] w-full'>
-        <img src="/discordIcon.png" onClick={() => {window.location.href= '/'}} className="pr-3 w-[50px]" />
-        <img src="/twitterIcon.png" onClick={() => {window.location.href= '/'}} className="pr-3 w-[50px]"/>
+    <div className='flex flex-row justify-end items-center p-12 h-[60px] w-full'>
+        <img src="/discordIcon.png" onClick={() => {
+          openInNewTab("https://discord.gg/AHWXpyaKCc");
+        }} className="mr-8 w-[45px] cursor-pointer" />
+        <img src="/twitterIcon.png" className="cursor-pointer" onClick={() => {
+          openInNewTab("https://twitter.com/useDiverseHQ");
+        }} className="w-[45px] cursor-pointer"/>
     </div>
   )
 }
