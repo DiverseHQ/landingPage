c01d71e507d91f625db528b92193e44c348ec1b0
kirtirajsinh <kirtiraj3738@gmail.com>
2022-07-04 22:12:19 +0530

    Correct Discord Link + font resize

--- components/CenterContent.js
+++ components/CenterContent.js
@@ -7,18 +7,18 @@ const CenterContent = () => {
     <div className="flex flex-col items-center min-h-[70vh] justify-center mb-[90px] relative">
       <img src="/vector-bg.png" className="max-h-[80vh] object-contain" />
       <div className="absolute">
-      <div className="text-5xl md:text-6xl flex items-center justify-center pt-16">
+      <div className="text-4xl md:text-6xl flex items-center justify-center pt-16">
       <div>
       <Typewriter
   options={{
     strings: ['Create', 'Comment', 'Socialize'],
     autoStart: true,
     loop: true,
-    wrapperClassName: 'text-purple-600',
+    wrapperClassName: 'text-purple-600 text-4xl md:text-6xl',
   }}
 />
 </div>
-<h1 className="text-5xl md:text-6xl">To Earn</h1>
+<h1 className="text-4xl md:text-6xl">To Earn</h1>
     </div>
    <div className="pt-4">
      <h1 className="text-2xl max-w-2xl text-center leading-10 p-1 text-slate-700"> Join communities, express your creativity, and get noticed for your work! </h1>
