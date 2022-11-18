609ca81c712079f01c1907488de4c3ec1f4857a2
Meta-K <kirtiraj3738@gmail.com>
2022-11-18 17:22:51 +0530

    Add footer

--- components/new/SocialAndFooter.js
+++ components/new/SocialAndFooter.js
@@ -1,8 +1,47 @@
 import React from 'react'
+import {FaDiscord, FaTelegramPlane} from "react-icons/fa"
+import {BsTwitter, BsInstagram} from "react-icons/bs"
 
 const SocialAndFooter = () => {
   return (
-    <div>SocialAndFooter</div>
+    <div className="flex flex-col">
+      <div className="flex  flex-row justify-around items-center  place-self-center w-2/3  border rounded-t-full  bg-purple-200 p-2">
+        <a><FaDiscord className="w-10 h-10 "/></a>
+        <a><BsTwitter className="h-9 w-9" /></a>
+        <a><BsInstagram className="w-9 h-9" /></a>
+        <a><FaTelegramPlane className="w-9 h-9" /></a> 
+      </div>
+
+    <div className="flex flex-row text-center bg-cyan-600 justify-start p-2">
+      <div className="items-center
+            justify-center mx-3 p-3">
+        <ul>
+          <h6 className="items-center mb-1 font-semibold">Quick Links</h6>
+        </ul>
+          <ul>
+            <a href="#">Home</a>
+          </ul>
+          <ul>
+            <a href="#">Blog</a>
+          </ul>
+          <ul>
+            <a href="#">Feature</a>
+          </ul>
+          <ul>
+            <a href="#">Whitepaper</a>
+          </ul>
+      </div>
+      <div className="items-center justify-center p-3" >
+        <ul>
+        <h6 className="items-center mb-1 font-semibold">Contack US</h6>
+        </ul>
+          <ul>
+            <a>diversehq.xyz@gmail.com</a>
+          </ul>
+      </div>
+        
+    </div>
+    </div>
   )
 }
 
--- components/new/TeamSection.js
+++ components/new/TeamSection.js
@@ -2,7 +2,33 @@ import React from 'react'
 
 const TeamSection = () => {
   return (
-    <div>TeamSection</div>
+    <div className="flex flex-col ">
+      <h1 className="place-self-center text-2xl font-semibold">Team Members</h1>
+      <section className="flex  items-center justify-center p-3 flex-wrap ">
+        <div className="flex flex-row border rounded p-1 m-2">
+          <img src="https://cdn.discordapp.com/attachments/1014407377056968754/1043118148926312528/WFDFwC_I_400x400.jpg" alt="avatar" className="w-20 h-20 border rounded-full" />
+          <div className="flex flex-col justify-center p-3 mx-2">
+          <h3 className="text-lg">Deven</h3>
+          <p>Creative dev</p>
+          </div>
+        </div>
+        <div className="flex flex-row border rounded p-1 m-2">
+          <img src="/Kirrttiraj.jpg" alt="avatar" className="w-20 h-20 border rounded-full" />
+          <div className="flex flex-col justify-center p-3 mx-2">
+          <h3 className="text-lg">Kirrttiraj</h3>
+          <p>Frontend dev</p>
+          </div>
+        </div>
+        <div className="flex flex-row border rounded p-1 m-2">
+          <img src="/Kirrttiraj.jpg" alt="avatar" className="w-20 h-20 border rounded-full" />
+          <div className="flex flex-col justify-center p-3 mx-2">
+          <h3 className="text-lg">Taha</h3>
+          <p>Community dev</p>
+          </div>
+        </div>
+       
+      </section>
+    </div>
   )
 }
 
