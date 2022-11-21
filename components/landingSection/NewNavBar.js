7b59d3fce87d69e166595c3bc8a2739522cef33c
RathodDeven <devenrathodrd@gmail.com>
2022-11-21 11:22:51 +0530

    z index

--- components/landingSection/NewNavBar.js
+++ components/landingSection/NewNavBar.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { GrMenu } from 'react-icons/gr'
-import {ImCross} from 'react-icons/Im'
+import { ImCross } from 'react-icons/Im'
 
 import LogoComponent from './LogoComponent'
 import useWindowSize from '../../utils/hook/useWindowSize'
@@ -10,7 +10,7 @@ const NewNavBar = ({ featureSectionRef, teamSectionRef }) => {
   const onClickFeatures = () => {
     featureSectionRef.current.scrollIntoView({ behavior: 'smooth' })
   }
-const [hambergerState, setHambergerState] = useState(false)
+  const [hambergerState, setHambergerState] = useState(false)
   const onClickTeam = () => {
     teamSectionRef.current.scrollIntoView({ behavior: 'smooth' })
   }
@@ -18,7 +18,7 @@ const [hambergerState, setHambergerState] = useState(false)
   return (
     <>
       {!isMobile && (
-        <div className="w-full flex flex-row justify-between items-center border-b border-[#999292] px-6 absolute z-10">
+        <div className="w-full flex flex-row justify-between items-center border-b border-[#999292] px-6 absolute z-30">
           <LogoComponent />
           <div className="flex flex-row items-center justify-center gap-x-11">
             <div className="text-xl hover:text-p-h hover:border-b-4 hover:border-p-h hover:mb-[-4px] py-6">
@@ -46,36 +46,40 @@ const [hambergerState, setHambergerState] = useState(false)
         </div>
       )}
       {isMobile && (
-        <div className="flex flex-row items-center justify-between h-14 bg-p-bg rounded-b-[20px] p-3 absolute w-full z-10">
+        <div className="flex flex-row items-center justify-between h-14 bg-p-bg rounded-b-[20px] p-3 absolute w-full z-30">
           <LogoComponent />
-          <nav>
+          <div>
             <section className="flex">
-              <button onClick={() => setHambergerState(!hambergerState)}><GrMenu className="w-6 h-6" /></button>
-              {
-                hambergerState && (
-
-              <nav className="h-screen  top-[0px] right-[0px] flex flex-col justify-around items-center w-full md:hidden bg-p-bg border z-60 duration-1000 absolute ">
-          <button className="self-end mr-10" onClick={() => setHambergerState(!hambergerState)}><ImCross className="w-6 h-6" /></button>
-          <a href="#" className="underline decoration-2 text-lg">
-            Community
-          </a>
-          <a href="#" className=" underline decoration- decoration-2 text-lg">
-            Team
-          </a>
-          <a href="#" className="underline decoration-2 text-lg">
-            Blog
-          </a>
-          <a href="#" className="underline decoration-2 text-lg">
-            Features
-          </a>
-          
-        </nav>
-                )
-              }
-
+              <button onClick={() => setHambergerState(!hambergerState)}>
+                <GrMenu className="w-6 h-6" />
+              </button>
+              {hambergerState && (
+                <div className="h-screen  top-[0px] right-[0px] flex flex-col justify-around items-center w-full md:hidden bg-p-bg border z-30 duration-1000 absolute ">
+                  <button
+                    className="self-end mr-10"
+                    onClick={() => setHambergerState(!hambergerState)}
+                  >
+                    <ImCross className="w-6 h-6" />
+                  </button>
+                  <a href="#" className="underline decoration-2 text-lg">
+                    Community
+                  </a>
+                  <a
+                    href="#"
+                    className=" underline decoration- decoration-2 text-lg"
+                  >
+                    Team
+                  </a>
+                  <a href="#" className="underline decoration-2 text-lg">
+                    Blog
+                  </a>
+                  <a href="#" className="underline decoration-2 text-lg">
+                    Features
+                  </a>
+                </div>
+              )}
             </section>
-          </nav>
-        
+          </div>
         </div>
       )}
     </>
