e0fbd09a058fa55b0c96d48dc9db2d1ea020f5d5
RathodDeven <devenrathodrd@gmail.com>
2022-11-26 12:17:45 +0530

    fixed _app.js eslint

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,46 @@
 import Head from 'next/head'
 import '../styles/globals.css'
 import { NextSeo } from 'next-seo'
+import PropTypes from 'prop-types'
 
-function MyApp ({ Component, pageProps }) {
-  return <>
-  <Head>
-    <title>DiverseHQ</title>
-  </Head>
-  <NextSeo
-  title="DiverseHQ"
-  description='Join communities, express your creativity, and get noticed for your work!'
-  openGraph={{
-    type: 'website',
-    locale: 'en_US',
-    url: 'https://diversehq.xyz',
-    site_name: 'DiverseHQ',
-    images: [
-      {
-        url: 'https://diversehq.xyz/vector-bg.png',
-        width: 1200,
-        height: 630,
-        alt: 'DiverseHQ'
-      }
-    ]
-  }}
-  twitter={{
-    handle: '@useDiverseHQ',
-    cardType: 'summary_large_image'
-  }}
-  />
-  <div className="text-black">
-  <Component {...pageProps} />
-  </div>
-  </>
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>DiverseHQ</title>
+      </Head>
+      <NextSeo
+        title="DiverseHQ"
+        description="Join communities, express your creativity, and get noticed for your work!"
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url: 'https://diversehq.xyz',
+          site_name: 'DiverseHQ',
+          images: [
+            {
+              url: 'https://diversehq.xyz/vector-bg.png',
+              width: 1200,
+              height: 630,
+              alt: 'DiverseHQ'
+            }
+          ]
+        }}
+        twitter={{
+          handle: '@useDiverseHQ',
+          cardType: 'summary_large_image'
+        }}
+      />
+      <div className="text-black">
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired
 }
 
 export default MyApp
