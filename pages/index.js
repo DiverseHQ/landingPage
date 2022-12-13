91fad56949140381b5c682156efea9c8dc2d6494
alirmemon37 <alirmemona@gmail.com>
2022-12-13 12:19:42 +0530

    Powered By Section added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import DiverseHQIntroDiv from '../components/new/DiverseHQIntroDiv'
+import PoweredBySection from '../components/new/PoweredBySection'
 import FeaturesSection from '../components/new/FeaturesSection'
 import LandingSection from '../components/new/LandingSection'
 import SocialAndFooter from '../components/new/SocialAndFooter'
@@ -18,6 +19,7 @@ export default function Home() {
         featureSectionRef={featureSectionRef}
         teamSectionRef={teamSectionRef}
       />
+      <PoweredBySection />
       <DiverseHQIntroDiv />
       <div className="relative">
         <div ref={featureSectionRef}>
